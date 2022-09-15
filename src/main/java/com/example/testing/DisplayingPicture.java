7356804b74a0df2741a9a3cfb579826936aca4d2
dionthorn <dionthorn@gmail.com>
2022-09-15 12:03:00 -0600

    example showing how to load a .bmp

---
+++ src/main/java/com/example/testing/DisplayingPicture.java
@@ -0,0 +1,30 @@
+package com.example.testing;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class DisplayingPicture extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("ImageView Experiment 1");
+
+        Image image = new Image("myFile.bmp");
+        ImageView imageView = new ImageView(image);
+
+        HBox hbox = new HBox(imageView);
+
+        Scene scene = new Scene(hbox, 200, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
